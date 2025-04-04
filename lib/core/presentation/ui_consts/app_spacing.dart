f22cf2b6a639fbcec91bbec81298d90707a07ece
Roman Godun <4l.roman.godun@gmail.com>
2025-04-04 22:40:33 +0300

    chore: chore

--- lib/core/presentation/ui_consts/app_spacing.dart
+++ lib/core/presentation/ui_consts/app_spacing.dart
@@ -3,5 +3,8 @@ class AppSpacing {
   static const double s = 10.0;
   static const double m = 20.0;
   static const double l = 30.0;
-  static const double xl = 45.00;
+  static const double xl = 40.00;
+  static const double xxl = 50.00;
+  static const double xxxl = 70.00;
+  static const double huge = 90.00;
 }
