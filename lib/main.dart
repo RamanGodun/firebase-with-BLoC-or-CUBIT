673f8f1e015bd99cc5c80bf3fef3bb3fc6a6af9e
Roman Godun <4l.roman.godun@gmail.com>
2025-04-04 15:58:37 +0300

    feat: add firebase conection

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// [FirebaseOptions] for use with your Firebase apps.
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // 🔍
+  debugPrint('Firebase apps: ${Firebase.apps}');
+
   runApp(const MainApp());
 }
 
